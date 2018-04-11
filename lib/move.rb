9a8194248d9b4970dd36ceeff225bbf403c38179
Gonzalo Esperon <gonzalo.esperon@wework.com>
2018-04-11 13:14:06 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,12 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  
+  user_input = gets.to_i
+end
+
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
 end
 
 def move()
