53647d858cea8151c3cb486a0e6ea72023cae93b
Gonzalo Esperon <gonzalo.esperon@wework.com>
2018-04-11 13:27:52 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,11 +12,10 @@ def input_to_index(user_input)
   converted_input = user_input.to_i
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
 
 def move()
-
+  board = [" ", " ", " "]
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
 end
